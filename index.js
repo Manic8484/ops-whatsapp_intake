9eaba3ae6e6ab97080c661ba5c1325cdc8a10e01
Manic8484 <84vfr800@gmail.com>
2026-09-17 13:33:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ app.get("/", (req, res) => {
   res.status(200).send("WhatsApp intake running");
 });
 
+function extractWhRef(text) {
+  if (!text) return null;
+
+  // Accept examples such as:
+  // WH-123
+  // WH123
+  // WH 123
+  // wh-123
+  const match = text.match(/\bWH[\s-]?(\d+)\b/i);
+
+  if (!match) return null;
+
+  return `WH-${match[1]}`;
+}
+
+function escapeXml(value) {
+  return String(value)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;");
+}
+
 app.post("/webhooks/whatsapp", (req, res) => {
   console.log("=== WhatsApp webhook received ===");
   console.log(JSON.stringify(req.body, null, 2));
@@ -21,6 +45,7 @@ app.post("/webhooks/whatsapp", (req, res) => {
     NumMedia,
     ProfileName,
     WaId,
+    MessageType,
     Forwarded,
     FrequentlyForwarded
   } = req.body;
@@ -33,14 +58,34 @@ app.post("/webhooks/whatsapp", (req, res) => {
     NumMedia,
     ProfileName,
     WaId,
+    MessageType,
     Forwarded,
     FrequentlyForwarded
   });
 
-  res
-    .status(200)
-    .type("text/xml")
-    .send("<Response></Response>");
+  const whRef = extractWhRef(Body);
+
+  let reply;
+
+  if (whRef) {
+    reply =
+      `${whRef} selected. ` +
+      `Send or forward messages, photos or documents for this consignment.`;
+  } else {
+    reply =
+      "I couldn't find a WH reference. " +
+      "Please send one, for example WH-123.";
+  }
+
+  console.log("Bot reply:", reply);
+
+  const twiml =
+    `<?xml version="1.0" encoding="UTF-8"?>` +
+    `<Response>` +
+    `<Message>${escapeXml(reply)}</Message>` +
+    `</Response>`;
+
+  res.status(200).type("text/xml").send(twiml);
 });
 
 const port = process.env.PORT || 8080;
